d4d594a0fca1fc5d9a225b8e1096b717d168060e
Felipe de Jesús Félix Arredondo <a00833150@tec.mx>
2025-05-25 09:48:20 -0600

    Manejo de los clusters

---
+++ estrategias_dict.py
@@ -0,0 +1,31 @@
+estrategias = [{'ID': 1, 'Nombre': 'BANAMEX CLABE TRADICIONAL', 'Costo': 1.75}, {'ID': 2, 'Nombre': 'BANAMEX CUENTA', 'Costo': 1.75}, {'ID': 3, 'Nombre': 'BANAMEX TARJETA', 'Costo': 1.75}, {'ID': 4, 'Nombre': 'BANAMEX INTERBANCARIO', 'Costo': 1.75}, {'ID': 5, 'Nombre': 'BANAMEX TRADICIONAL REINTENTO', 'Costo': 1.75}, {'ID': 6, 'Nombre': 'BBVA TRADICIONAL REINTENTO', 'Costo': 8.0}, {'ID': 7, 'Nombre': 'BANORTE TRADICIONAL REINTENTO ', 'Costo': 2.5}, {'ID': 8, 'Nombre': 'SANTANDER TRADICIONAL REINTENTO ', 'Costo': 1.97}, {'ID': 9, 'Nombre': 'SANTANDER CLABE TRADICIONAL', 'Costo': 2.82}, {'ID': 10, 'Nombre': 'SANTANDER CUENTA ', 'Costo': 2.2}, {'ID': 11, 'Nombre': 'SANTANDER CLABE INTERBANCARIO', 'Costo': 3.18}, {'ID': 12, 'Nombre': 'BANORTE CLABE TRADICIONAL', 'Costo': 2.5}, {'ID': 13, 'Nombre': 'BANORTE CUENTA TRADICIONAL', 'Costo': 2.5}, {'ID': 14, 'Nombre': 'BANORTE CLABE INTERBANCARIO', 'Costo': 4.5}, {'ID': 15, 'Nombre': 'BANORTE CLABE EN LINEA', 'Costo': 2.5}, {'ID': 16, 'Nombre': 'BANORTE CUENTA EN LINEA', 'Costo': 2.5}, {'ID': 17, 'Nombre': 'BBVA CLABE TRADICIONAL', 'Costo': 8.0}, {'ID': 18, 'Nombre': 'BBVA CUENTA TRADICIONAL', 'Costo': 8.0}, {'ID': 19, 'Nombre': 'BBVA CLABE MATUTINO', 'Costo': 8.0}, {'ID': 20, 'Nombre': 'BBVA CUENTA MATUTINO', 'Costo': 8.0}, {'ID': 21, 'Nombre': 'BBVA CLABE EN LINEA', 'Costo': 4.0}, {'ID': 22, 'Nombre': 'BBVA CUENTA EN LINEA', 'Costo': 4.0}, {'ID': 23, 'Nombre': 'BBVA CLABE PARCIAL', 'Costo': 1.6}, {'ID': 24, 'Nombre': 'BBVA CUENTA PARCIAL', 'Costo': 1.6}, {'ID': 25, 'Nombre': 'BBVA CLABE INTERBANCARIA', 'Costo': 6.0}]
+
+
+cambios = {
+    0 : {'Nombre': 'NO SE MANDAN', 'Costo': 0, 'Optional': 0},
+    1 : {'Nombre': 'BANAMEX CLABE TRADICIONAL', 'Costo': 1.75, 'Optional': 1.75},
+    2 : {'Nombre': 'BANAMEX CUENTA', 'Costo': 1.75, 'Optional': 1.75},
+    3 : {'Nombre': 'BANAMEX TARJETA', 'Costo': 1.75, 'Optional': 1.75},
+    4 : {'Nombre': 'BANAMEX INTERBANCARIO', 'Costo': 1.75, 'Optional': 1.75},
+    5 : {'Nombre': 'BANAMEX TRADICIONAL REINTENTO', 'Costo': 1.75, 'Optional': 1.75},
+    6 : {'Nombre': 'BBVA TRADICIONAL REINTENTO', 'Costo': 8.0, 'Optional': 0},
+    7 : {'Nombre': 'BANORTE TRADICIONAL REINTENTO ', 'Costo': 2.5, 'Optional': 2.5},
+    8 : {'Nombre': 'SANTANDER TRADICIONAL REINTENTO ', 'Costo': 1.97, 'Optional': 1.97},
+    9 : {'Nombre': 'SANTANDER CLABE TRADICIONAL', 'Costo': 2.82, 'Optional': 2.82},
+    10 : {'Nombre': 'SANTANDER CUENTA ', 'Costo': 2.2, 'Optional': 2.2},
+    11 : {'Nombre': 'SANTANDER CLABE INTERBANCARIO', 'Costo': 3.18, 'Optional': 3.18},
+    12 : {'Nombre': 'BANORTE CLABE TRADICIONAL', 'Costo': 2.5, 'Optional': 2.5},
+    13 : {'Nombre': 'BANORTE CUENTA TRADICIONAL', 'Costo': 2.5, 'Optional': 2.5},
+    14 : {'Nombre': 'BANORTE CLABE INTERBANCARIO', 'Costo': 4.5, 'Optional': 4.5},
+    15 : {'Nombre': 'BANORTE CLABE EN LINEA', 'Costo': 2.5, 'Optional': 2.5},
+    16 : {'Nombre': 'BANORTE CUENTA EN LINEA', 'Costo': 2.5, 'Optional': 2.5},
+    17 : {'Nombre': 'BBVA CLABE TRADICIONAL', 'Costo': 8.0, 'Optional': 0},
+    18 : {'Nombre': 'BBVA CUENTA TRADICIONAL', 'Costo': 8.0, 'Optional': 0},
+    19 : {'Nombre': 'BBVA CLABE MATUTINO', 'Costo': 8.0, 'Optional': 0},
+    20 : {'Nombre': 'BBVA CUENTA MATUTINO', 'Costo': 8.0, 'Optional': 0},
+    21 : {'Nombre': 'BBVA CLABE EN LINEA', 'Costo': 4.0, 'Optional': 0},
+    22 : {'Nombre': 'BBVA CUENTA EN LINEA', 'Costo': 4.0, 'Optional': 0},
+    23 : {'Nombre': 'BBVA CLABE PARCIAL', 'Costo': 1.6, 'Optional': 0},
+    24 : {'Nombre': 'BBVA CUENTA PARCIAL', 'Costo': 1.6, 'Optional': 0},
+    25 : {'Nombre': 'BBVA CLABE INTERBANCARIA', 'Costo': 6.0, 'Optional': 0}
+}
